d39d831685d9f1a8edd0e56fc4e1f5ee0723526a
Blue Gaston <bbluegaston@gmail.com>
2020-04-03 09:34:58 -0700

    updated to reworked upstream

--- pxp-runtime-builder/build.sh
+++ pxp-runtime-builder/build.sh
@@ -3,5 +3,5 @@ headsha=$(git rev-parse --verify HEAD)
 MODE=$1
 
 echo $MODE
-docker build --build-arg MODE=$MODE -t pxp-runtime-builder:$headsha --build-arg CACHEBUST=$(date +%s) . 
+docker build --build-arg MODE=$MODE -t pxp-runtime-builder:$headsha --build-arg CACHEBUST=$(date +%s) .  
 
